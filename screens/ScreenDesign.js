05c7f3d0a6874f9c4d1068ea0bb408fae901c89d
emaob7 <emanuelob7@gmail.com>
2024-07-12 12:02:58 -0400

    design image2

--- screens/ScreenDesign.js
+++ screens/ScreenDesign.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { View,Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
 
 const designData = [
-  { id: '1', image: require('../assets/cvd/cesar.png') },
-  { id: '2', image: require('../assets/cvd/hardvard.png') },
+  { id: '1', image: require('../assets/cvd/moon.png') },
+  { id: '2', image: require('../assets/cvd/cesar.png') },
+  { id: '3', image: require('../assets/cvd/general.png') },
+  { id: '4', image: require('../assets/cvd/hardvard.png') },
   // Añade más diseños aquí
 ];
 
@@ -31,19 +33,19 @@ return(
     <View style={styles.gallery}>
          <TouchableOpacity style={styles.comp}  onPress={() => handleButtonPress(1)}>
          <Text style={styles.text}>Moon</Text>
-         <Image source={designData[1].image} style={styles.thumbnail} />
+         <Image source={designData[0].image} style={styles.thumbnail} />
       </TouchableOpacity>
       <TouchableOpacity style={styles.comp} onPress={() => handleButtonPress(2)}>
       <Text style={styles.text}>Cesar</Text>
-      <Image source={designData[0].image} style={styles.thumbnail} />
+      <Image source={designData[1].image} style={styles.thumbnail} />
       </TouchableOpacity>
       <TouchableOpacity style={styles.comp} onPress={() => handleButtonPress(3)}>
         <Text style={styles.text}>General</Text>
-        <Image source={designData[0].image} style={styles.thumbnail} />
+        <Image source={designData[2].image} style={styles.thumbnail} />
       </TouchableOpacity>
       <TouchableOpacity style={styles.comp} onPress={() => handleButtonPress(4)}>
         <Text style={styles.text}>Harvard</Text>
-        <Image source={designData[1].image} style={styles.thumbnail} />
+        <Image source={designData[3].image} style={styles.thumbnail} />
       </TouchableOpacity>
     </View>
 )
@@ -55,19 +57,20 @@ const styles = StyleSheet.create({
     comp:{
         width:"40%",
         marginHorizontal:10,
-        marginBottom:14,
+        marginBottom:19,
         //padding:10
        
       },
       gallery: {
         marginTop:10,
+        marginLeft:-10,
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
         flexWrap: 'wrap',
       },
       thumbnail: {
-        width: 150,
+        width: 165,
         height: 237,
         margin: 10,
         borderColor:"#CCCCCC",
